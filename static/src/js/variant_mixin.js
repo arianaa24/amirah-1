66e3bbd61d3b5b45e6918764c20debe05c1400bf
arianaa24 <arianacarrillo24@gmail.com>
2022-10-14 22:45:51 +0000

    Errores en website

--- static/src/js/variant_mixin.js
+++ static/src/js/variant_mixin.js
@@ -12,7 +12,8 @@ VariantMixin._onChangeCombination = function (ev, $parent, combination) {
         method: 'existencias',
         args: [[], combination.product_id]
     }).then(function (result) {
-        $message.after( "<strong>" + result + "</strong>" );
+        $( ".mensaje" ).remove();
+        $message.after( "<strong class='mensaje'>" + result + "</strong>" );
     });
 
     originalOnChangeCombination.apply(this, [ev, $parent, combination]);
